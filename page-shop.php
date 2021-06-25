536d76f553458c4919a62f5d2528490a70ef603c
arsua-greg <gregarsua@bpoc.co.jp>
2021-06-26 01:02:39 +0800

    mobile slider nlng kuwang

--- page-shop.php
+++ page-shop.php
@@ -10,7 +10,6 @@
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css" integrity="sha512-yHknP1/AwR+yx26cB1y0cjvQUMvEa2PFzt1c9LlS4pRQ5NOTZFWbhBig+X9G9eYW/8m0/4OXNx8pxJ6z57x0dw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
 </head>
 
 <body>
@@ -56,75 +55,44 @@
             </div>
         </div>
     </div>
-
     </div>
 
-    <div class="carousel" style="padding-top: 5em;">
-        <div class="carousel-cell">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Eddie Kinsella</p>
-            <p class="b">Call</p>
-            <p class="content"></p>
-        </div>
-        <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Harrison Benac</p>
-            <p class="b">Call</p>
-        </div>
-        <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Sean Kingston</p>
-            <p class="b">Call</p>
-        </div>
-        <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Hayden Kaupu</p>
-            <p class="b">Call</p>
-        </div>
-        <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Franklin Tudor</p>
-            <p class="b">Call</p>
-        </div>
-        <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Herbert Simpson</p>
-            <p class="b">Call</p>
-        </div>
-        <div class="carousel-cell"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" class="p">
-            <p class="n">Shaun Coleman</p>
-            <p class="b">Call</p>
-        </div>
+    <div class="yogurt">
+        <p class="yogurt__title">Yogurt Everyday!</p>
+        <p class="yogurt__desc">Subscribe for a weekly yogurt supply</p>
+        <p class="yogurt__choose">Choose your choice of yogurt flavors for a week and<br>
+            we will deliver it to you weekly! </p>
+        <button class="yogurt__btn">
+            Subscribe Now
+        </button>
     </div>
 
+    <div class="order">
+        <p class="order__title">Order our Yogurts</p>
 
-    <script type="text/javascript" src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <div class="order__logos">
+            <div class="foodpanda">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/foodpanda.png" alt="">
+            </div>
+            <div class="width_2"></div>
+            <div class="grab">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/grab.png" alt="">
+            </div>
+        </div>
+        <button class="order__btn">
+            See Nearby Distributors
+        </button>
+    </div>
+
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
     <script type="text/javascript">
-        $(document).ready(function() {
-            $(".b").click(function() {
-                $(this).toggleClass("b");
-                $(this).toggleClass("b-selected");
-            });
-        });
-
-        var elem = document.querySelector('.carousel');
-        var flkty = new Flickity(elem, {
-            // options
-            cellalign: 'right',
-            pageDots: false,
-            groupCells: '20%',
-            selectedAttraction: 0.03,
-            friction: 0.15
-        });
-        var flkty = new Flickity('.carousel', {
-            // options
-        });
-    </script>
-
-    <script type="text/javascript">
         $('.slider').slick({
+            autoplay: true,
             dots: true,
-            infinite: false,
+            infinite: true,
             speed: 300,
             slidesToShow: 4,
             slidesToScroll: 4,
