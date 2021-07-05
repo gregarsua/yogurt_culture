1a2ec9ce97d7164609a3df28d501961f444d9cf2
EJ Anton S. Potot <ejanton.potot@sun-asterisk.com>
2021-07-05 11:48:37 +0800

    added nav bar

--- page-start.php
+++ page-start.php
@@ -6,8 +6,78 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Yogurt Culture</title>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css" />
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/reset.css" />
+    <link rel="stylesheet" href="/assets/css/style.css" />
+    <link rel="stylesheet" href="/assets/css/reset.css" />
+    <!-- CSS only /////// -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <!-- ////////////////////////////// -->
+
+    <style>
+        nav {
+            box-shadow: 3px 5px 6px #ccc;
+        }
+
+
+        .navbar-light .navbar-toggler {
+            border: none;
+        }
+
+        .navbar-toggler:focus {
+            box-shadow: none;
+        }
+
+        .navbar-light .navbar-toggler-icon {
+            background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' width='30' height='30' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(12, 85, 160, 1)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='3' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
+        }
+
+
+        .pl-px-36 {
+            padding-left: 36px;
+        }
+
+        .pl-px-16 {
+            padding-left: 16px;
+        }
+
+        .nav-title {
+            color: #0C55A0;
+            font-weight: 600;
+            font-size: 11px;
+            font-family: 'Montserrat';
+        }
+
+        .image {
+            height: 40px;
+            width: 40px;
+        }
+
+        .pt-px-35 {
+            padding-top: 35px;
+        }
+
+        .pb-px-36 {
+            padding-bottom: 36px;
+        }
+
+        .yc-nav {
+            background: #0C55A0;
+            margin-top: .5rem;
+            margin-bottom: -.5rem;
+        }
+
+        a.nav-link {
+            color: white !important;
+        }
+
+        .center {
+            text-align: center;
+        }
+
+        .shop-icon{
+            padding: .25rem .75rem;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -17,47 +87,130 @@
             10% OFF ON ALL ITEMS
         </p>
     </div>
+    <!-- ////////////////////////////// -->
+
+    <!-- PARA PAGE-shop ni -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent z-index-10">
+        <!-- Diri lang usba para sa katong page-step 2,3,4 etc. -->
+        <div class="pl-px-36">
+            <img src="/assets/img/step-1.png" class="image" alt="" /><span class="pl-px-16 nav-title">LET’S START</span>
+        </div>
+        <!--  -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i>
+            </span>
+        </button>
+
+        <div class="collapse navbar-collapse yc-nav" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto pl-px-36 pt-px-35 pb-px-36">
+                <li class="nav-item text-white active">
+                    <a class="nav-link" href="#">Yogurt Culture</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Subscribe</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+    <!--  -->
+    <br>
+    <br>
+    <!--  -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-transparent z-index-10">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#nav2" aria-controls="nav2" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon">
+                <i class="fa fa-navicon" style="color:#fff; font-size:28px;"></i>
+            </span>
+        </button>
+        <a href="" class="center">
+            <img class="section_1__logo" src="/assets/img/logo.png" alt="" />
+        </a>
+        <a href="" class="shop-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="27.91" height="26.284" viewBox="0 0 27.91 26.284">
+                <path id="Path_8" data-name="Path 8" d="M480.985,163.45h14.047c.1,0,.208,0,.312,0a.745.745,0,0,1-.007,1.488c-.266.013-.534,0-.86,0a2.775,2.775,0,0,1,.583,2.488,2.5,2.5,0,0,1-.923,1.407,2.69,2.69,0,0,1-3.393-.039c-1.121-.961-1.231-2.184-.334-3.839h-5.776a2.712,2.712,0,0,1,.538,2.556,2.613,2.613,0,0,1-1.069,1.451,2.675,2.675,0,0,1-3.4-.337,2.715,2.715,0,0,1-.116-3.686H480.2c-.908,0-1.2-.413-.887-1.272q.39-1.083.779-2.166c.031-.086.053-.175.09-.3-.137,0-.247,0-.356,0a.9.9,0,0,1-.954-.8c-.194-1.039-.381-2.079-.572-3.118q-.8-4.343-1.6-8.686c-.126-.683-.259-1.365-.373-2.05a.364.364,0,0,0-.271-.321q-1.684-.642-3.36-1.3a.9.9,0,0,1-.649-1.166.832.832,0,0,1,1.023-.58c.641.206,1.265.462,1.893.7.774.3,1.541.616,2.318.907a1.039,1.039,0,0,1,.7.868q1.072,5.875,2.156,11.747c.12.652.239,1.3.363,1.98.1.007.186.02.276.02q6.8,0,13.592.007a.323.323,0,0,0,.369-.268q1.495-4.689,3-9.375c.025-.077.041-.157.068-.261-.429-.023-.839-.049-1.25-.066-2.2-.092-4.4-.178-6.6-.272q-4.125-.176-8.25-.363a.874.874,0,0,1-.871-.707.845.845,0,0,1,.4-.932,1.2,1.2,0,0,1,.593-.129c2.927.117,5.853.249,8.78.374q3.254.139,6.508.269c.612.025,1.225.041,1.837.083.568.039.873.267.945.728a1.381,1.381,0,0,1-.072.6q-1.544,4.862-3.1,9.72c-.164.513-.327,1.028-.492,1.541a1,1,0,0,1-1.159.848q-6.469,0-12.937-.007a.418.418,0,0,0-.478.343C481.457,162.168,481.222,162.786,480.985,163.45Zm1.665,2.1a1.182,1.182,0,0,0-1.232,1.1,1.188,1.188,0,1,0,2.37.149A1.162,1.162,0,0,0,482.65,165.552Zm11.006,1.2a1.194,1.194,0,0,0-2.387-.056,1.194,1.194,0,1,0,2.387.056Z" transform="translate(-472.002 -143.142)" fill="#0c55a0" />
+            </svg>
+        </a>
+
+
+
+        <div class="collapse navbar-collapse yc-nav" id="nav2">
+            <ul class="navbar-nav mr-auto pl-px-36 pt-px-35 pb-px-36">
+                <li class="nav-item text-white active">
+                    <a class="nav-link" href="#">Yogurt Culture</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Shop</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Subscribe</a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <!-- ////////////////////////////// -->
 
     <div class="section_1">
         <a href="">
-            <img class="section_1__logo" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="" />
+            <img class="section_1__logo" src="/assets/img/logo.png" alt="" />
         </a>
 
         <ul class="section_1__list">
             <li class="section_1__step-1">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/step-1.png" alt="" />
+                <img src="/assets/img/step-1.png" alt="" />
                 <p class="step-1">LET’S START</p>
             </li>
             <li class="step-dot">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/dot.png" alt="" />
+                <img src="/assets/img/dot.png" alt="" />
             </li>
             <li class="section_1__step-2">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/step-2.png" alt="" />
+                <img src="/assets/img/step-2.png" alt="" />
                 <p class="step-2">SELECT PLAN</p>
             </li>
             <li class="step-dot">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/dot.png" alt="" />
+                <img src="/assets/img/dot.png" alt="" />
             </li>
             <li class="section_1__step-3">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/step-3.png" alt="" />
+                <img src="/assets/img/step-3.png" alt="" />
                 <p class="step-3">SELECT ITEMS</p>
             </li>
             <li class="step-dot">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/dot.png" alt="" />
+                <img src="/assets/img/dot.png" alt="" />
             </li>
             <li class="section_1__step-4">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/step-4.png" alt="" />
+                <img src="/assets/img/step-4.png" alt="" />
                 <p class="step-4">CHECKOUT</p>
             </li>
         </ul>
     </div>
-
     <div class="section_2">
         <h2 class="section_2__title">WELP! WE NEED TO START SOMEWHERE.</h2>
 
         <div class="section_2__col">
             <div class="section_2__col__col-1">
-                <img class="kalorie" src="<?php echo get_template_directory_uri(); ?>/assets/img/kalorie.png" alt="" />
+                <img class="kalorie" src="/assets/img/kalorie.png" alt="" />
             </div>
 
             <div class="section_2__col__col-2">
@@ -108,6 +261,11 @@
         <p class="footer__txt">Terms and Privacy | Disclaimer </p>
         <p class="footer__txt2">© YOGURT CULTURE 2021</p>
     </footer>
+    <!-- ////////////////////////////// -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <!-- ////////////////////////////// -->
 
     <script>
         function startPlan() {
